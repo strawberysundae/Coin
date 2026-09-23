96475e2349e85f4057fef6b0285fd139acccae33
Sandy Yang <sandyy16@nycstudents.net>
2026-09-23 15:27:07 -0400

    update

--- Coin.java
+++ Coin.java
@@ -1,3 +1,14 @@
 public class Coin {
-
+    private String state;
+    private int heads;
+    private int tails;
+    
+    public String getState() {
+        return state;
+    }
+    public void flip() {
+        if (Math.random() < 0.5)
+            state = "tails";
+        else state = "heads";
+    }
 }
--- Main.java
+++ Main.java
@@ -3,5 +3,8 @@ public class Main {
 
         Coin penny = new Coin();
         System.out.println(penny);
+        System.out.println(penny.getState());
+        penny.flip();
+        System.out.println(penny.getState());
     }
 }
